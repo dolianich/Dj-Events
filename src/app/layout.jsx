e51bd9e356cce154807d0496ac7ef4afc91de19e
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-05 20:59:46 +0300

    Navbar and Footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from './layout.module.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({title, keywords, description, children}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        <div className={styles.container}>
+          {children}
+        </div>
+        <Footer/>
+      </body>
     </html>
   );
 }
